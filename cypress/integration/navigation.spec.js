3d674a9bb2aa2c0b832361e6c3616220b1430aa2
Kershia1 <insatiable.meerkat@gmail.com>
2024-03-09 15:02:17 -0800

    More cy tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,14 @@
+{
+  "env": {
+    "cypress/globals": true;
+  }
+}
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
